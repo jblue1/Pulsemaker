5b0b18d2a84a739889d02ca54d9f6ebe524d9d03
jblue1 <joblue@davidson.edu>
2019-11-14 22:10:44 -0500

    Added layers to CNN and combo

--- build_net.py
+++ build_net.py
@@ -13,14 +13,14 @@ def build_FCNN():
 
 def build_Conv1D():
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv1D(16, 20, activation='relu', input_shape=(500, 1, )))
+    model.add(tf.keras.layers.Conv1D(64, 20, activation='relu', input_shape=(500, 1, )))
     model.add(tf.keras.layers.MaxPooling1D(3))
-    model.add(tf.keras.layers.Conv1D(32, 10, activation='relu'))
+    model.add(tf.keras.layers.Conv1D(64, 20, activation='relu'))
     model.add(tf.keras.layers.MaxPooling1D(3))
-    model.add(tf.keras.layers.Conv1D(64, 5, activation='relu'))
+    model.add(tf.keras.layers.Conv1D(364, 20, activation='relu'))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(500, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.3))
+    #model.add(tf.keras.layers.Dropout(0.3))
     model.add(tf.keras.layers.Dense(2))
 
     return model
@@ -30,31 +30,28 @@ def build_combo():
     inputDense = tf.keras.Input(shape=(500,))
     inputConv = tf.keras.Input(shape=(500, 1,))
 
-
     x = tf.keras.layers.Dense(500, activation='relu')(inputDense)
     x = tf.keras.layers.Dense(500, activation='relu')(x)
-    x = tf.keras.layers.Dense(300, activation='relu')(x)
-    x = tf.keras.layers.Dense(300, activation='relu')(x)
+    x = tf.keras.layers.Dense(500, activation='relu')(x)
 
-    y = tf.keras.layers.Conv1D(16, 20, activation='relu')(inputConv)
-    y = tf.keras.layers.MaxPooling1D(3)(y)
-    y = tf.keras.layers.Conv1D(32, 10, activation='relu')(y)
-    y = tf.keras.layers.MaxPooling1D(3)(y)
-    y = tf.keras.layers.Conv1D(64, 5, activation='relu')(y)
+    y = tf.keras.layers.Conv1D(64, 20, activation='relu')(inputConv)
+    y = tf.keras.layers.MaxPooling1D(2)(y)
+    y = tf.keras.layers.Conv1D(64, 20, activation='relu')(y)
+    y = tf.keras.layers.MaxPooling1D(2)(y)
+    y = tf.keras.layers.Conv1D(64, 20, activation='relu')(y)
+    y = tf.keras.layers.MaxPooling1D(2)(y)
     y = tf.keras.layers.Flatten()(y)
     y = tf.keras.layers.Dense(500)(y)
 
     concat = tf.keras.layers.concatenate([x, y])
-    out = tf.keras.layers.Dense(300, activation='relu')(concat)
-    out = tf.keras.layers.Dense(300, activation='relu')(out)
-    out = tf.keras.layers.Dense(300, activation='relu')(out)
+    out = tf.keras.layers.Dense(500, activation='relu')(concat)
     out = tf.keras.layers.Dense(2)(out)
 
     return tf.keras.Model([inputDense, inputConv], out)
 
 
 def main():
-    model = build_Conv1D()
+    model = build_combo()
     print(model.summary())
 
 
